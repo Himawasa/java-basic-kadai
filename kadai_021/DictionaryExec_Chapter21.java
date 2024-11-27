1232485103254186db8fd7c91a5d0675feabe00a
Hiroshi.I <himawasa@gmail.com>
2024-11-27 21:43:42 +0900

    課題021再提出2回目

--- kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -2,16 +2,14 @@ package kadai_021;
 
 public class DictionaryExec_Chapter21 {
 
-    public static void main(String[] args) {
-        // 辞書クラスのインスタンスを作成
-        Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
+	public static void main(String[] args) {
+		// 辞書クラスのインスタンスを作成
+		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
 
-        // 調べる英単語を配列にセット
-        String[] wordsToSearch = new String[] {"apple", "banana", "grape", "orange"};
+		// 検索対象の英単語を配列にセット
+		String[] targetWord = new String[] { "apple", "banana", "grape", "orange" };
 
-        // 配列を使って辞書を検索
-        for (int i = 0; i < wordsToSearch.length; i++) {
-            System.out.println(dictionary.searchWord(wordsToSearch[i]));
-        }
-    }
+		// 辞書クラスに処理を依頼
+		dictionary.searchWord(targetWord);
+	}
 }
--- kadai_021/Dictionary_Chapter21.java
+++ kadai_021/Dictionary_Chapter21.java
@@ -4,29 +4,36 @@ import java.util.HashMap;
 
 public class Dictionary_Chapter21 {
 
-    // HashMapの宣言と初期化（キー: 英単語, 値: 意味）
-    private HashMap<String, String> dictionary = new HashMap<>();
+	// HashMapの宣言と初期化（キー: 英単語, 値: 意味）
+	private HashMap<String, String> dictionary = new HashMap<>();
 
-    // 辞書に単語を追加
-    public Dictionary_Chapter21() {
-        dictionary.put("apple", "りんご");
-        dictionary.put("peach", "もも");
-        dictionary.put("banana", "バナナ");
-        dictionary.put("lemon", "レモン");
-        dictionary.put("pear", "梨");
-        dictionary.put("kiwi", "キウイ");
-        dictionary.put("strawberry", "いちご");
-        dictionary.put("grape", "ぶどう");
-        dictionary.put("muscat", "マスカット");
-        dictionary.put("cherry", "さくらんぼ");
-    }
+	// コンストラクタで辞書に単語を追加
+	public Dictionary_Chapter21() {
+		dictionary.put("apple", "りんご");
+		dictionary.put("peach", "もも");
+		dictionary.put("banana", "バナナ");
+		dictionary.put("lemon", "レモン");
+		dictionary.put("pear", "梨");
+		dictionary.put("kiwi", "キウイ");
+		dictionary.put("strawberry", "いちご");
+		dictionary.put("grape", "ぶどう");
+		dictionary.put("muscat", "マスカット");
+		dictionary.put("cherry", "さくらんぼ");
+	}
 
-    // フロー図のメソッド
-    public String searchWord(String word) {
-        if (dictionary.containsKey(word)) {
-            return word + "の意味は" + dictionary.get(word);
-        } else {
-            return word + "は辞書に存在しません";
-        }
-    }
+	// 配列全体を受け取り、繰り返し処理して結果を出力するメソッド
+	public void searchWord(String[] targetWord) {
+		for (int i = 0; i < targetWord.length; i++) {
+			System.out.println(findWord(targetWord[i]));
+		}
+	}
+
+	// 単語を検索し、結果を返すメソッド
+	public String findWord(String word) {
+		if (dictionary.containsKey(word)) {
+			return word + "の意味は" + dictionary.get(word);
+		} else {
+			return word + "は辞書に存在しません";
+		}
+	}
 }
