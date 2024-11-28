cae374a2d1541f33d1fedf160023ba906f479620
Hiroshi.I <himawasa@gmail.com>
2024-11-28 18:48:26 +0900

    課題026初回提出

---
+++ kadai_026/JyankenExec_Chapter26.java
@@ -0,0 +1,11 @@
+package kadai_026;
+
+public class JyankenExec_Chapter26 {
+    public static void main(String[] args) {
+        // じゃんけんクラスのインスタンスを作成
+        Jyanken_Chapter26 jyanken = new Jyanken_Chapter26();
+
+        // じゃんけんをプレイ
+        jyanken.playGame();
+    }
+}
---
+++ kadai_026/Jyanken_Chapter26.java
@@ -0,0 +1,63 @@
+package kadai_026;
+
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Jyanken_Chapter26 {
+
+    // 自分のじゃんけんの手を入力する
+    public String getMyChoice() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("自分のじゃんけんの手を入力しましょう");
+        System.out.println("グーはrockのrを入力しましょう");
+        System.out.println("チョキはscissorsのsを入力しましょう");
+        System.out.println("パーはpaperのpを入力しましょう");
+
+        return scanner.nextLine();
+    }
+
+    // 対戦相手のじゃんけんの手をランダムで選ぶ
+    public String getYourChoice() {
+        Random random = new Random();
+        int randomChoice = random.nextInt(3); // 0, 1, 2のいずれかを生成
+
+        String choice;
+        switch (randomChoice) {
+            case 0 -> choice = "r"; // グー
+            case 1 -> choice = "s"; // チョキ
+            case 2 -> choice = "p"; // パー
+            default -> choice = ""; // 念の為
+        }
+        return choice;
+    }
+
+    // じゃんけんを行う
+    public void playGame() {
+        // 自分の手
+        String myChoice = getMyChoice();
+        // 対戦相手の手
+        String yourChoice = getYourChoice();
+
+        // HashMapでじゃんけんの手を定義
+        HashMap<String, String> hands = new HashMap<>();
+        hands.put("r", "グー");
+        hands.put("s", "チョキ");
+        hands.put("p", "パー");
+
+        // 手を表示
+        System.out.println("自分の手は" + hands.get(myChoice) + "，対戦相手の手は" + hands.get(yourChoice));
+
+        // 勝敗を判定
+        if (myChoice.equals(yourChoice)) {
+            System.out.println("あいこです");
+        } else if ((myChoice.equals("r") && yourChoice.equals("s")) || // グー vs チョキ
+                (myChoice.equals("s") && yourChoice.equals("p")) || // チョキ vs パー
+                (myChoice.equals("p") && yourChoice.equals("r")) // パー vs グー
+        ) {
+            System.out.println("自分の勝ちです");
+        } else {
+            System.out.println("自分の負けです");
+        }
+    }
+}
